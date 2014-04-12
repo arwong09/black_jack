6476350b73ec48511e9fb78afd4aa5047c38f9e0
Andrew Wong <arwong09@gmail.com>
2014-04-11 22:03:53 -0700

    initial commit

---
+++ hand.rb
@@ -0,0 +1,52 @@
+class Hand
+  # This is called a *factory method*; it's a *class method* that
+  # takes the a `Deck` and creates and returns a `Hand`
+  # object. This is in contrast to the `#initialize` method that
+  # expects an `Array` of cards to hold.
+  def self.deal_from(deck)
+    Hand.new(deck.take(2))
+  end
+
+  attr_accessor :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def points
+    sum = 0
+    @cards.each do |card|
+      if card.value == :ace
+        sum > 10 ? sum += 1 : sum += 11
+      else
+        sum += card.blackjack_value
+      end
+    end
+    sum
+  end
+
+  def busted?
+    points > 21
+  end
+
+  def hit(deck)
+    raise "already busted" if busted?
+    @cards += deck.take(1)
+  end
+
+  def beats?(other_hand)
+    return false if busted?
+    return true if other_hand.busted?
+    return true if points > other_hand.points
+    return false
+  end
+
+  def return_cards(deck)
+    deck.return(@cards)
+    @cards = []
+  end
+
+  def to_s
+    @cards.join(",") + " (#{points})"
+  end
+end
